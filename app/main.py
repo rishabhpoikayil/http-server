ba04742848208443c2bb5fe115acf5e5af408390
Rishabh Poikayil <rishabhpoikayil@Rishabhs-MacBook-Pro.local>
2024-06-13 19:08:20 -0700

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ import socket
 
 
 def main():
-    # print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
 
