3a1dddc950251cf7037eac496da42d099207abc4
Rishabh Poikayil <rishabhpoikayil@Rishabhs-MacBook-Pro.local>
2024-06-13 19:19:26 -0700

    second part done

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 import socket
 
-
 def main():
-    print("logs")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    print("Server is listening on localhost:4221")
 
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection from {client_address}")
+        client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        # client_socket.close()
 
 if __name__ == "__main__":
     main()
