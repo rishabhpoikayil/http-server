43c4c5a53015e903cb2e6ceee5fed166aff09d18
Rishabh Poikayil <rishabhpoikayil@Rishabhs-MacBook-Pro.local>
2024-06-14 07:44:33 -0700

    test

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import socket
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    addr = ("localhost", 4221)
+    server_socket = socket.create_server(addr, reuse_port=True)
     print("Server is listening on localhost:4221")
 
     while True:
         client_socket, client_address = server_socket.accept()
-        print(f"Connection from {client_address}")
+        print(f"Connection from {client_address}{client_socket}")
         client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
         # client_socket.close()
 
